04fc68a3497a19687529839398e1a9d37ad4d15e
Lorenzo <lorenzoburgi@gmail.com>
2021-07-25 17:41:32 -0300

    Primer commit

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
-# Buscando el mejor producto 
-Para esta misión necesitamos crear una web, usando HTML, CSS y Javascript, que utilice la API de Mercado Libre para buscar entre los resultados el mejor productos. 
-Y ¿cómo vamos a definir el mejor producto? Bueno, eso va a quedar en cada implementación. Cada uno de ustedes que acepte este desafío debe establecer un criterio y elegir entre todos los resultados que les ofrece la API, uno de los resultados y mostrarlo en su webapp. Una vez que elijan el mejor producto simplemente muestran la info básica y linkean al articulo en la web de Mercado Libre.
+_Link pagina_
 
-Recuerden enviar sus [PRs](https://docs.github.com/en/github/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/about-pull-requests) y si publican sus webs (una alternativa es usar [GitHub Pages](https://pages.github.com/) suban una foto de su web (y la URL) a Instagran y nos mencionan https://www.instagram.com/apx.school/ para compartirles el proyecto.
+Hola! soy Lorenzo y les comparto mi resultado del desafio planteado en instagram! Justo este mes estoy cursando el modulo 4 y quise aprovecharlo como una oportunidad para practicar!
 
-## Requisitos
-Para encarar esta misión necesitan dominio básicos de HTML, CSS y Javascript (**módulo 4** de [la carrera](https://apx.school/carreras/dwf)) . Algunos de estos conocimientos básicos son:
-- Fetch: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
-- DOM: https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
-- CSS Grids - https://css-tricks.com/snippets/css/complete-guide-grid/
-- Flexbox - https://css-tricks.com/snippets/css/a-guide-to-flexbox/
-- Semántica - https://developer.mozilla.org/en-US/docs/Glossary/Semantics#semantics_in_html
-
-## Recursos
-
-#### Documentación de la API de Mercado Libre
-https://developers.mercadolibre.com.ar/en_us/items-and-searches#Get-items-by-a-search-query
-
-#### Diseño de referencia
-https://www.figma.com/file/9c8fPdTHLDZ4h1ZvK0WNg0/misi%C3%B3n-buscando-el-mejor-producto?node-id=0%3A1
-
-#### Código
-Este repo tiene algunos archivos que pueden tomar como base pero es totalmente opcional.
+El buscador entrega el mejor producto siguiendo el criterio de: "producto con mas ventas", ya que cuando compro algo en ML siempre me guío en base a eso. Tiene un par de errores los cuales no arreglé por falta de tiempo (Como por ejemplo optimizarlo para mobile) pero dentro de todo es funcional. Espero sus opiniones para saber en que mejorar :)
--- index.js
+++ index.js
@@ -1,12 +1,98 @@
-// Esta función debería alterar el HTML
-// para mostrar el restulado elegido en la pantalla
-function dibujarElMejorProducto() {}
+function Remover() {
+  // Obtengo todos (ALL) los productos si hay
+  const productosEnPantalla = document.querySelectorAll(".result-item");
 
-function elegirElMejorProducto(listaDeProductos) {}
+  // Si no hay ninguno, no devuelvo nada
+  if (productosEnPantalla.length == 0) {
+    return;
+  }
 
-function buscarProductos(palabraABuscar) {}
+  // Si hay, los borro
+  productosEnPantalla.forEach((carta) => carta.remove());
+}
 
-// inicia todo e invoca al resto de las funciones
-function main() {}
+function mostSelledItem(items) {
+  let vendidos = 0;
+  var itemMasVendido;
 
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].sold_quantity > vendidos) {
+      vendidos = items[i].sold_quantity;
+      itemMasVendido = items[i];
+    }
+  }
+  return mostrarResultados(itemMasVendido);
+}
+
+function mostrarResultados(element) {
+  const contenedor = document.querySelector(".results"); //Etiqueta "results" que si es visible
+  const template = document.querySelector("#result-item-template"); //invoco al template
+
+  Remover(); //remuevo elementos buscados si los hay
+
+  console.log(element);
+  //itero con un foreach a los resultados que me entrega la API
+  const titleElement = template.content.querySelector(".result-item-title"); //invoco al title del template
+  titleElement.textContent = element.title; //Le cambio el title al template por el title que me entrega la api
+
+  function condicionTraductor(condicion) {
+    //Como la API de ML me entrega "New" o "Used" utilizo esta funcion para traducirlos al español
+    if (condicion == "new") {
+      return "Nuevo";
+    } else {
+      return "Usado";
+    }
+  }
+
+  const conditionElement = template.content.querySelector(
+    //invoco a la condicion del item
+    ".result-item-condition"
+  );
+  conditionElement.textContent = condicionTraductor(element.condition); //Le sobreescribo la condicion que me entrega la API
+
+  function AskFreeShipping(param) {
+    if (param == true) {
+      return "Envio gratis!";
+    }
+  }
+
+  const freeShipping = template.content.querySelector(".shipping");
+  freeShipping.textContent = AskFreeShipping(element.shipping.free_shipping);
+
+  const linkRef = template.content.querySelector(".link-prod"); //invoco al link del item
+  linkRef.href = element.permalink; //Le sobreescribo el link
+
+  const priceElement = template.content.querySelector(".result-item-price"); //invoco a la etiqueta price
+  priceElement.textContent = "$" + element.price; //le sobreescribo el precio que me entrega la API
+
+  const cantidadVendidosElement = template.content.querySelector(
+    //invoco a la etiqueta cantidad de elementos vendidos
+    ".result-item-sell-count-number"
+  );
+  cantidadVendidosElement.textContent = element.sold_quantity; //le sobreescribo a la cantidad que me entrega la API
+
+  const imgElement = template.content.querySelector(".result-img"); //invoco a la imagen
+  imgElement.src = element.thumbnail; //sobreescribo la imagen default por la que me entrega la API
+
+  const clone = document.importNode(template.content, true); //Creo un clon del template para luego introducirlo al contenedor que es la etiqueta results
+  contenedor.appendChild(clone); //Introduzco la info del template al contenedor visible, creando la "CARTA" con la info que me dió la API
+}
+
+function main() {
+  const myFormEl = document.querySelector(".mi-form");
+  myFormEl.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    const palabraABuscar = evento.target.producto.value; //Recibo la palabra que escriba en el buscador
+
+    fetch("https://api.mercadolibre.com/sites/MLA/search?q=" + palabraABuscar)
+      .then(function (res) {
+        return res.json(); //entrega otra promesa
+      })
+      .then(function (data) {
+        mostSelledItem(data.results); //le paso la los resultados de la data que me da ML a la funcion "mostrarResultados"
+      });
+  });
+}
+
+//EJECUTO el main
 main();
